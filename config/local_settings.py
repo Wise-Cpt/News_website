027955e5c18d2632df157314f38125cd92fccd27
Wise-Cpt <sudo-install-me@protonmail.com>
2023-01-25 11:09:29 +0100

    new gitignore

--- config/local_settings.py
+++ config/local_settings.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^cdjre+d8%r$##xm*9kg2juy*3x(!lvyd#c0en)ki_u*9b#_vb"
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
